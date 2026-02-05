307d8da3bd7e1994c16e0885fe1b8a381c0d75b7
Anton Trochuk <trochuk19@gmail.com>
2026-02-05 21:21:48 +0400

    2.16. Начинаем программировать

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import Main from './components/main';
+
+function App () {
+  return (
+    <Main numberRental={312} />
+  );
+}
+
+export default App;
---
+++ src/type.tsx
@@ -0,0 +1,12 @@
+type MainRentalProp = {
+  numberRental: number;
+}
+
+type OfferCardType = {
+  price: number;
+  image: string;
+  name: string;
+  type: string;
+};
+
+export type { MainRentalProp, OfferCardType };
