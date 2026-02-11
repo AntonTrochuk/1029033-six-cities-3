db5bf53c1d6aa1a39255e08e8ff02ccd67706590
Anton Trochuk <trochuk19@gmail.com>
2026-02-11 16:32:16 +0400

    3.19. Маршрут по тайным тропам

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,29 @@
+import {Routes, Route} from 'react-router-dom';
 import Main from './components/main';
+import NotFound from './404';
 import {RENTAL_COUNT} from './constants';
+import Login from './components/login';
+import Favorites from './components/favorites';
+import Offer from './components/offer';
+import PrivateRoute from './components/private-route';
 
 function App() {
-  return <Main rentalCount={RENTAL_COUNT} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Main rentalCount={RENTAL_COUNT} />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/favorites"
+        element={
+          <PrivateRoute>
+            <Favorites />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/offer/:id" element={<Offer />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
