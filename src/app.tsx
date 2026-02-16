9d56f35078bd00e3124c9130adb4c3bbd3bdcef7
Anton Trochuk <trochuk19@gmail.com>
2026-02-16 19:40:44 +0400

    Задание от наставника

--- src/app.tsx
+++ src/app.tsx
@@ -6,21 +6,45 @@ import Login from './components/login';
 import Favorites from './components/favorites';
 import Offer from './components/offer';
 import PrivateRoute from './components/private-route';
+import PageLayout from './components/page-layout/PageLayout';
 
 function App() {
   return (
     <Routes>
-      <Route path={AppRoute.MAIN} element={<Main rentalCount={RENTAL_COUNT} />} />
-      <Route path={AppRoute.LOGIN} element={<Login />} />
+      <Route
+        path={AppRoute.MAIN}
+        element={
+          <PageLayout divClass="page page--gray page--main" mainClass="page__main page__main--index">
+            <Main rentalCount={RENTAL_COUNT} />
+          </PageLayout>
+        }
+      />
+      <Route
+        path={AppRoute.LOGIN}
+        element={
+          <PageLayout divClass="page page--gray page--login" mainClass="page__main page__main--login">
+            <Login />
+          </PageLayout>
+        }
+      />
       <Route
         path={AppRoute.FAVORITES}
         element={
-          <PrivateRoute>
-            <Favorites />
-          </PrivateRoute>
+          <PageLayout divClass="page" mainClass="page__main page__main--favorites">
+            <PrivateRoute>
+              <Favorites />
+            </PrivateRoute>
+          </PageLayout>
         }
       />
-      <Route path={AppRoute.OFFER} element={<Offer />} />
+      <Route
+        path={AppRoute.OFFER}
+        element={
+          <PageLayout divClass="page" mainClass="page__main page__main--offer">
+            <Offer />
+          </PageLayout>
+        }
+      />
       <Route path={AppRoute.NOT_FOUND} element={<NotFound />} />
     </Routes>
   );
